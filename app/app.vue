f47cfdb453a8f11ee16ca9621dd742499196ef90
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2024-11-30 17:02:52 +0300

    chore: add placeholder data to app

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,30 @@
 <script lang="ts" setup>
-const data = {};
+const data = {
+    type: "FeatureCollection",
+    features: [
+        {
+            type: "Feature",
+            geometry: {
+                type: "LineString",
+                coordinates: [
+                    [-75.0, 40.0, -5],
+                    [-75.0, 40.1, -5],
+                    [-75.1, 40.1, -5],
+                ],
+            },
+            properties: {
+                typeofpipe: "hot",
+                radius: 7,
+                depth: 5,
+                date_last_fix: "2024-01-02",
+                date_need_fix: "2025-10-17",
+                material: "Стальная",
+                status: "Используется",
+                comment: "я короч изолентой замотал, надеюсь не прорвет",
+            },
+        },
+    ],
+} as const;
 </script>
 
 <template>
